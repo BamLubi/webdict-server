6f11208d2e5e1ccbbc7ba38790b988756fc57d63
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 14:01:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # 查找是否存在已有的进程
-echo "Finding exist Process"
+echo "Finding exist Process..."
 webdict_server_pid=`ps -ef | grep webdict_server | grep -v "grep" | awk '{print $2}'`
 for id in $webdict_server_pid
 do
     kill -9 $id
     echo "Killed $id"
 done
+echo "Finding exist Process done"
+
+echo "Running..."
 
 # 编译
 # go build ./src/main/main.go
