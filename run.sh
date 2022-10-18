c8f108c69828da520c0fe6ae2913f78dc8526183
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 13:54:50 +0800

    fix: update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# 查找是否存在已有的进程
+echo "Finding exist Process"
+webdict_server_pid=`ps -ef | grep webdict_server | grep -v "grep" | awk '{print $2}'`
+for id in $webdict_server_pid
+do
+    kill -9 $id
+    echo "Killed $id"
+done
+
 # 编译
-go build ./src/main/main.go
+# go build ./src/main/main.go
+
 # 赋权限
-chmod 777 main
+chmod 777 webdict_server
 
 # 设置生产环境
 export GIN_MODE=release
+
 # 运行
-./main
+./webdict_server
