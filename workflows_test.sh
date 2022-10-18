9941d639dd9f12b93f155196b11dd4ee3c9045c7
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 16:46:06 +0800

    Update workflows_test.sh

--- workflows_test.sh
+++ workflows_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -uroot -pustc123123 -e "CREATE DATABASE webdict DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+mysql -h 127.0.0.1 -P 3306 -uroot -pustc123123 -e "CREATE DATABASE webdict DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
