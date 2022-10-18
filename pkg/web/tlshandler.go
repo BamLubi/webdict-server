20a96eebeebd15fa76d9d0c5318047dd1deb6d03
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 21:21:42 +0800

    fix

--- pkg/web/tlshandler.go
+++ pkg/web/tlshandler.go
@@ -9,7 +9,7 @@ func TlsHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		secureMiddleware := secure.New(secure.Options{
 			SSLRedirect: true,
-			SSLHost:     "127.0.0.1:8000",
+			SSLHost:     ":8000",
 		})
 		err := secureMiddleware.Process(c.Writer, c.Request)
 
