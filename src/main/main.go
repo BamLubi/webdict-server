73139bd61ce0f4003646896bb9d29165ef517143
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 19:39:19 +0800

    fix: allow prod-test
    without run http server, when running workflows

--- src/main/main.go
+++ src/main/main.go
@@ -13,6 +13,9 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "-DEV" {
 		mode = "DEVELOPMENT"
 		viper.SetConfigName("dev")
+	} else if len(os.Args) > 1 && os.Args[1] == "-PROD_TEST" {
+		mode = "PRODUCTION TEST"
+		viper.SetConfigName("app")
 	} else {
 		mode = "PRODUCTION"
 		viper.SetConfigName("app")
@@ -25,6 +28,11 @@ func main() {
 	// 初始化logger
 	config.InitLogger(path, level)
 	config.Logger.Info("Server Run Success on Mode(" + mode + ")")
+
+	// 如果是线上测试版，则直接退出，不需要运行服务器
+	if mode == "PRODUCTION TEST" {
+		return
+	}
 
 	// 启动服务器
 	web.RunHttp()
