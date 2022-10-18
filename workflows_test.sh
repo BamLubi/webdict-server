9b5b91c493b7c828bebf554ede744dd3610647a1
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 16:42:02 +0800

    test: init mysql in workflows

--- workflows_test.sh
+++ workflows_test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mysql -uroot -pustc123123
-CREATE DATABASE webdict DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-exit
+mysql -uroot -pustc123123 -e "CREATE DATABASE webdict DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
