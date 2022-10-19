d744754dabd6c16f8f176966fad571b43924d95e
bamlubi <1126458447@qq.com>
2022-10-19 21:13:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 
 # 查找是否存在已有的进程
 echo "Finding exist Process..."
-webdict_server_pid=`ps -ef | grep webdict_server | grep -v "grep" | awk '{print $2}'`
+# shellcheck disable=SC2009
+webdict_server_pid=$(ps -ef | grep webdict_server | grep -v "grep" | awk '{print $2}')
 for id in $webdict_server_pid
 do
-    kill -9 $id
+    kill -9 "$id"
     echo "Killed $id"
 done
 echo "Finding exist Process done"
